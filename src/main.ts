2a6df58a63d4ccc5eda786a8412f80388ea2df1c
Sal Rahman <salehen.rahman@gmail.com>
2023-01-07 11:02:43 -0800

    Managed to plot something

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,76 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+const canvas = document.createElement("canvas") satisfies HTMLCanvasElement;
+document.body.appendChild(canvas);
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const context = canvas.getContext("2d");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (!context) {
+	throw new Error("Failed to get context");
+}
+
+type Point2D = readonly [number, number];
+
+type Range = {
+	from: Point2D;
+	to: Point2D;
+};
+
+const range = {
+	from: [-2, 0.8],
+	to: [2, -0.8],
+} satisfies Range;
+
+function getDelta([from, to]: [number, number]): number {
+	const range = [from, to].sort();
+	return range[1] - range[0];
+}
+
+function ratio([from, to]: [number, number], value: number) {
+	const [start] = [from, to].sort();
+	const ratio = (value - start) / getDelta([from, to]);
+	return ratio;
+}
+
+function getPixelPoint(
+	{ from, to }: Range,
+	dimension: Point2D,
+	point: Point2D
+): Point2D {
+	console.assert(dimension[0] > 0);
+	console.assert(dimension[1] > 0);
+
+	const xRatio = ratio([from[0], to[0]], point[0]);
+	const yRatio = ratio([from[1], to[1]], point[1]);
+
+	return [xRatio * dimension[0], dimension[1] - yRatio * dimension[1]];
+}
+
+console.log(
+	getPixelPoint(
+		range,
+		[
+			canvas.getBoundingClientRect().width,
+			canvas.getBoundingClientRect().height,
+		],
+		[-2, 0.8]
+	)
+);
+
+const max = 100;
+
+const { width, height } = canvas.getBoundingClientRect();
+
+const fn = (x: number) => x ** 3 + 0.25 * x ** 2 - x;
+
+for (let i = 0; i < max; i++) {
+	const deltaX = getDelta([range.from[0], range.to[0]]);
+	const x = (i / max) * deltaX + range.from[0];
+
+	const y = fn(x);
+
+	const point = getPixelPoint(range, [width, height], [x, y]);
+
+	context.fillStyle = "black";
+	context.fillRect(point[0], point[1], 2, 2);
+}
+
+export {};
